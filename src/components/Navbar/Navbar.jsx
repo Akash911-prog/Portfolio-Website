ffabbce60ca3fb4bce73782f089442ee7a6fcc58
Akash <Akashsamanta0571@gmail.com>
2025-08-25 23:30:36 +0530

    dark mode toggle fully complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between px-2'>
             <div className="name text-2xl m-2 font-bold">Akash Samanta</div>
-            <nav className="links hidden sm:flex justify-between gap-4 bg-white/10 border-b border-white/10 px-3 py-1 rounded-xl mt-1 ">
+            <nav className="links hidden sm:flex justify-between gap-4 bg-glass/10 border-b border-glass/10 px-3 py-1 rounded-xl mt-1 ">
                 <span className='links-items'>About</span>
                 <span className='links-items'>Experience</span>
                 <span className='links-items'>Projects</span>
@@ -21,7 +21,7 @@ const Navbar = () => {
 
             <div className="btns flex gap-3 sm:hidden">
 
-                <ThemeToggle />
+                <ThemeToggle size={24} />
 
                 <button className={`hamburger-icon cursor-pointer z-10 ${open ? "open" : ""}`} onClick={() => setOpen(!open)}>
                     <svg className='cursor-pointer' width="24" height="24" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,23 +1,33 @@
 "use client";
 import { useTheme } from "@/contexts/ThemeProvider";
+import { useEffect, useState } from "react";
 import './ThemeToggle.css'
 
-export default function ThemeToggle() {
+export default function ThemeToggle({
+    size = 24, //change this to change the icon size
+}) {
     const { theme, setTheme } = useTheme();
+    const [clientSize, setClientSize] = useState(size);
+    console.log(size);
+
+    useEffect(() => {
+        setClientSize(size);
+    }, [size]);
 
     return (
         <label
             htmlFor="theme-toggle"
-            className="wrapper flex items-center justify-center gap-2 cursor-pointer"
+            className="wrapper cursor-pointer"
+            style={{ "--size": `${clientSize}px` }}
         >
             <input
                 id="theme-toggle"
                 className="hidden"
                 type="checkbox"
-                checked={theme === "dark"}
+                checked={theme === "light"}
                 onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
             />
-            <div className="icon" />
+            <div className="icon size-0.5" onClick={() => setTheme(theme === "light" ? "dark" : "light")} />
         </label>
     );
 }
